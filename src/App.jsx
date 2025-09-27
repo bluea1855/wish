be62fb67c03be07b8a715dd790629e09098b1064
bluea1855 <fakemail1855@gmail.com>
2025-09-27 16:49:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import Home from "./pages/Home";
 import FeatureContent from "./pages/FeatureContent";
 import Gift from "./pages/Gift";
 import DynamicRedirect from './pages/[id]';
-import { Analytics } from "@vercel/analytics";
-import { SpeedInsights } from "@vercel/speed-insights";
+// import { Analytics } from "@vercel/analytics";
+// import { SpeedInsights } from "@vercel/speed-insights";
 
 function App() {
   return (
@@ -18,8 +18,8 @@ function App() {
       <Route path="/gift" element={<FeatureContent />} />
       <Route path="/:id" element={<DynamicRedirect />} />
     </Routes>
-    <Analytics/>
-    <SpeedInsights/>
+    {/* <Analytics/> */}
+    {/* <SpeedInsights/> */}
     </>
   );
 }
