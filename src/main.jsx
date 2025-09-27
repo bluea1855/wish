0ac1717eac277d3b4e94406a92b4c59a7a6de022
bluea1855 <fakemail1855@gmail.com>
2025-09-27 16:51:57 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import './index.css';
-import { Analytics } from "@vercel/analytics";
-import { SpeedInsights } from "@vercel/speed-insights";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
